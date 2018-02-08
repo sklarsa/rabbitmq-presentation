aaa437492c66d51b8890cee77372729d03ddca9a
Steven Sklar <steve@predata.com>
2018-02-08 15:36:50 -0500

    Ties together pika example

--- amqp_example.py
+++ amqp_example.py
@@ -1,3 +1,20 @@
+import pika
+
+EXCHANGE_NAME = 'foo'
+QUEUE_NAME = 'bar'
+
+
+def init_rabbitmq():
+    connection = pika.BlockingConnection()
+    channel = connection.channel()
+
+    channel.exchange_declare(EXCHANGE_NAME, "topic")
+    channel.queue_declare(QUEUE_NAME, durable=True)
+    channel.queue_bind(QUEUE_NAME, EXCHANGE_NAME, "*")
+
+    connection.close()
+
+
 class Producer(object):
 
     def __init__(self, connection):
@@ -16,8 +33,23 @@ class Consumer(object):
 
     def get_message(self, queue):
         channel = self.connection.channel()
-        # channel.queue_declare ...
         method_frame, header_frame, body = channel.basic_get(queue)
         if method_frame:
             channel.basic_ack(method_frame.delivery_tag)
         return body
+
+
+def hello_world():
+    init_rabbitmq()
+
+    conn = pika.BlockingConnection()
+
+    p = Producer(conn)
+    p.send_message("Hello World!", EXCHANGE_NAME, "test")
+
+    c = Consumer(conn)
+    print(c.get_message(QUEUE_NAME))
+
+
+if __name__ == "__main__":
+    hello_world()
