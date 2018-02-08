795323a4a151cbf7bb89150e12016c0fbbcd5293
Steven Sklar <steve@predata.com>
2018-02-08 17:03:07 -0500

    Makes length a mandatory arg to tasks

--- celery_example_1/run.py
+++ celery_example_1/run.py
@@ -1,7 +1,7 @@
 from tasks import fit_neural_net, run_user_query
 
 for x in range(4):
-    fit_neural_net.delay()
+    fit_neural_net.delay(10)
 
 for x in range(40):
-    run_user_query.delay()
+    run_user_query.delay(0.1)
--- celery_example_1/tasks.py
+++ celery_example_1/tasks.py
@@ -3,12 +3,13 @@ from main import app
 
 
 @app.task
-def fit_neural_net(length=15):
+def fit_neural_net(length):
     print("Fitting neural net...")
     time.sleep(length)
+    print("Done fitting neural net...")
 
 
 @app.task
-def run_user_query(length=0.1):
+def run_user_query(length):
     print("Running user query...")
     time.sleep(length)
