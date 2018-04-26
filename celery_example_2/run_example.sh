cf8ff78a6e5a6a6e255e7d5dc9b17484b1825e7e
Steven Sklar <steve@predata.com>
2018-04-26 14:30:22 -0400

    Adjust example

--- celery_example_2/run_example.sh
+++ celery_example_2/run_example.sh
@@ -1,6 +1,6 @@
 celery worker --app main --concurrency 1 -Q webapp -n worker-2 &
 pid1=$!
-celery worker --app main --concurrency 3 -Q neural_net -n worker-3 &
+celery worker --app main --concurrency 3 -n worker-3 &
 pid2=$!
 
 python run.py
